a99df16b59e8ddaca2fe689f9cb6aeeb309ae273
Yash Khati <yashkhati23@gmail.com>
2024-10-22 09:51:41 +0530

    Created Pages templates and Routs for them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>Home</h1>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/projects' element={<Projects />} /> 
+    </Routes>
+  </BrowserRouter>
   )
 }
