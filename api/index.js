cd58a31c92a251c67eb207828dc82d8ec678f314
Yash Khati <yashkhati23@gmail.com>
2024-10-22 12:12:38 +0530

    Connection made with MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.URI)
+    .then(()=>{
+        console.log("DB Connected")
+    })
+    .catch((err) => {
+        console.log("DB Connection Error : ",err)
+    })
 
 const app = express();
 
