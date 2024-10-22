2285caa94f83925ae32109003251ef69ea9778ad
Yash Khati <yashkhati23@gmail.com>
2024-10-22 11:32:42 +0530

    Header Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,13 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
   <BrowserRouter>
+    <Header/>
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
