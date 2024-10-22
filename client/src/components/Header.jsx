cc2283de4bb723f56ea1a0a2a02a20b1228b1c3b
Yash Khati <yashkhati23@gmail.com>
2024-10-22 11:35:28 +0530

    Signin Button Modified in Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header()
                 <FaMoon/>
             </Button>
             <Link to='/sign-in'>    
-                <Button gradientDuoTone='purpleToBlue'>
+                <Button gradientDuoTone='purpleToBlue' outline>
                     SignIn
                 </Button>
             </Link>
