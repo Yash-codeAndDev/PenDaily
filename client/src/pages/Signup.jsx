eea1709f94921fc06ce39076ac455a6d067a7d60
Yash Khati <yashkhati23@gmail.com>
2024-10-24 20:05:48 +0530

    added functionality to sign up page  (loading, handleSubmit() )

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,57 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-export default function Signup() {
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link , useNavigate} from 'react-router-dom'
+
+
+
+import { HiInformationCircle } from "react-icons/hi";
+
+
+export default function Signup() 
+{
+  const [formData, setFormData] = useState({});
+  const [errorMesssage,setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+  const handleChange = (e) => 
+  {
+    setFormData({...formData, [e.target.id]:e.target.value.trim()})
+  };
+
+  // console.log(formData)
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      // if fields are empty
+      if(!formData.username || !formData.email || !formData.password){
+          return setErrorMessage('Please fill all fields')
+      }
+
+        const res = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        
+        if(data.success === false){
+          return setErrorMessage(data.message);
+        }
+        setLoading(false)
+        if(res.ok){
+          navigate('/sign-in')
+        }
+    } catch (error) {
+        setErrorMessage(error.message)
+        setLoading(false)
+    }
+  }
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center'>
@@ -20,34 +70,44 @@ export default function Signup() {
 
 
         <div className='flex-1'>
-            <form className='flex flex-col gap-4' >
+            <form className='flex flex-col gap-4'  onSubmit={handleSubmit}>
               <div className=''>
                 <Label value='Your Username' />
                 <TextInput 
                   type='text'
                   placeholder='Username'
                   id="username"
+                  onChange={handleChange}
                 />
               </div>
               <div className=''>
                 <Label value='Your email' />
                 <TextInput 
-                  type='text'
+                  type='email'
                   placeholder='abc@xyz.com'
                   id="email"
+                  onChange={handleChange}
                 />
               </div>
               <div className=''>
                 <Label value='Your password' />
                 <TextInput 
-                  type='text'
+                  type='password'
                   placeholder='Password'
                   id="password"
+                  onChange={handleChange}
                 />
               </div>
 
-              <Button gradientDuoTone='purpleToPink' type='submit' outline>
-                Sign Up
+              <Button gradientDuoTone='purpleToPink' type='submit' outline disabled={loading}>
+                {
+                  loading ? (
+                    <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                    </>
+                  ) : 'Sign Up'
+                }
               </Button>
             </form>
 
@@ -56,9 +116,16 @@ export default function Signup() {
                 <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
             </div>
 
+            {
+              errorMesssage && (
+                <Alert className='items-center mt-5 italic rounded-lg' color='failure' icon={HiInformationCircle} >
+                  {errorMesssage}
+                </Alert>
+              )
+            }
+
         </div>
-      
-      
+
       </div>
     </div>
   )
